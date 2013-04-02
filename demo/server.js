e79efa11f8cbd6ad62b01db7aca0d1d502372253
Sebastian Seilund <sebastian@seilund.com>
2013-04-02 13:07:24 -0700

    Fixed demo server

--- demo/server.js
+++ demo/server.js
@@ -3,12 +3,12 @@ var fs = require('fs'),
 
 var app = express();
 
-app.use('/dist', express.static(__dirname + '/dist'));
-app.use('/demo', express.static(__dirname + '/demo'));
-app.use('/vendor', express.static(__dirname + '/vendor'));
+app.use('/dist', express.static(__dirname + '/../dist'));
+app.use('/demo', express.static(__dirname + '/../demo'));
+app.use('/vendor', express.static(__dirname + '/../vendor'));
 
 app.use(function(req, res, callback){
-    fs.readFile(__dirname + '/demo/index.html', 'utf8', function(err, html){
+    fs.readFile(__dirname + '/index.html', 'utf8', function(err, html){
         if (err) return callback(err);
         res.send(html);
     });
