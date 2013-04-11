6b679f33f84938c8d810c0ac07833981b6f69444
Sebastian Seilund <sebastian@seilund.com>
2013-04-11 00:07:29 -0700

    Added demo as a git submodule, so its easier to test stuff on your local machine

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
@@ -41,11 +43,56 @@ module.exports = function(grunt) {
         }
     });
 
-//    grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['concat', 'uglify', 'compass']);
+    
+    grunt.registerTask('demo', 'Copy to demo', function() {
+        var done = this.async();
+        // Run some sync stuff.
+        grunt.log.writeln('Copying to demo...');
+        var a = 0;
+        copyFile('dist/ember-animated-outlet.js', 'demo/public/js/vendor/ember-animated-outlet.js', function(err) {
+            if (err) throw err;
+            a++;
+            if (a == 2) {
+                done();
+            }
+        });
+        copyFile('dist/ember-animated-outlet.css', 'demo/public/js/vendor/ember-animated-outlet.css', function(err) {
+            if (err) throw err;
+            a++;
+            if (a == 2) {
+                done();
+            }
+        });
+    });
+    
+    grunt.registerTask('default', ['concat', 'uglify', 'compass', 'demo']);
 
 };
+
+function copyFile(source, target, callback) {
+    var cbCalled = false;
+
+    var rd = fs.createReadStream(source);
+    rd.on("error", function(err) {
+        done(err);
+    });
+    var wr = fs.createWriteStream(target);
+    wr.on("error", function(err) {
+        done(err);
+    });
+    wr.on("close", function(ex) {
+        done();
+    });
+    rd.pipe(wr);
+
+    function done(err) {
+        if (!cbCalled) {
+            callback(err);
+            cbCalled = true;
+        }
+    }
+}
