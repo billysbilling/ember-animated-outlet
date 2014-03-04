3103148ec5d60bfa7bb0306d41b570235b9cc9db
loostro <loostro@gmail.com>
2014-03-04 02:01:42 +0100

    Fix animation

--- src/js/effects/zoom.js
+++ src/js/effects/zoom.js
@@ -3,32 +3,28 @@
 var zoom = function(ct, newView, oldView, callback, direction) {
     var ctEl = ct.$(),
         newEl = newView.$(),
-        duration = 450;
-
-    ctEl.addClass('ember-animated-container-zoom-ct')
-    
-    oldView.addClass('ember-animated-container-zoom-'+direction+'-old');
-    newEl.addClass('ember-animated-container-zoom-'+direction+'-new');
+        oldEl = oldView.$();
+    ctEl.addClass('ember-animated-container-zoom-'+direction+'-ct');
+    newEl.addClass('ember-animated-container-zoom-new');
+    oldEl.addClass('ember-animated-container-zoom-old');
     
     setTimeout(function() {
         ctEl.addClass('ember-animated-container-zoom-ct-zooming');
         setTimeout(function() {
-            ctEl.removeClass('ember-animated-container-zoom-'+direction+'-ct');            
+            ctEl.removeClass('ember-animated-container-zoom-'+direction+'-ct');
             ctEl.removeClass('ember-animated-container-zoom-ct-zooming');
-            newEl.removeClass('ember-animated-container-zoom-'+direction+'-new');
-            setTimeout(function() {
-                callback();
-            }, 0);
-        }, duration);
+            newEl.removeClass('ember-animated-container-zoom-new');
+            callback();
+        }, 450);
     }, 0);
 };
 
 Ember.AnimatedContainerView.registerEffect('zoomIn', function(ct, newView, oldView, callback) {
-    slide(ct, newView, oldView, callback, 'in');
+    zoom(ct, newView, oldView, callback, 'in');
 });
 
 Ember.AnimatedContainerView.registerEffect('zoomOut', function(ct, newView, oldView, callback) {
-    slide(ct, newView, oldView, callback, 'out');
+    zoom(ct, newView, oldView, callback, 'out');
 });
 
 })();
